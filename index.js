645378187a17fe4e86e818e4560306d95052f818
Ziaul Islam <zia.isl@outlook.com>
2023-05-13 15:11:09 -0500

    working

--- index.js
+++ index.js
@@ -12,23 +12,61 @@ async function getPriceFeed(){
         });
         const $ = cheerio.load(data);
         const elemSelector = '#__next > div > div.main-content > div.cmc-body-wrapper > div > div:nth-child(1) > div.sc-beb003d5-2.bkNrIb > table > tbody > tr';
-
+        const indexName = [
+            'rank',
+            'name',
+            'price',
+            '1h',
+            '24h',
+            '7D',
+            'marketCap',
+            'volume',
+            'circulatingSupply'
+        ];
+        const coinAr = [];
         $(elemSelector).each((parentIdx, parentElem) =>{
             //console.log(parentElem);
+            let keyIdx = 0;
+            const coinObj = {};
             if(parentIdx <= 9){
                 $(parentElem).children().each((childIdx, childElem)=>{
-                    const tdValue = $(childElem).text();
+                    let tdValue = $(childElem).text(); 
+                    if((keyIdx == 1) || (keyIdx == 6)){
+                        tdValue = ($('p:first-child', $(childElem).html()).text());
+                    }
                     if(tdValue){
-                        console.log(tdValue);
+                        //console.log(tdValue);
+                        coinObj[indexName[keyIdx]] = tdValue;
+                        keyIdx++;
                     }
                 })
+                coinAr.push(coinObj);
             }
         })
-        //console.log($);
+        return coinAr;
     }
     catch(err){
         console.error(err);
     }
 }
 
-getPriceFeed();
+const app = express();
+
+app.get('/api/price-feed', async(req, res) => {
+    try{
+        const priceFeed = await getPriceFeed();
+
+        return res.status(200).json({
+            result : priceFeed
+        });
+    }
+    catch(err){
+        return res.status(500).json({
+            err: err.toString()
+        });
+    }
+})
+
+app.listen(3000, ()=>{
+    console.log('Crypto price feed running on port 3000');
+})
